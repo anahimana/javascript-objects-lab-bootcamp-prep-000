cc855319acfed813afb8a4405e135e205f92c37a
Alex Nahimana <alex.nahimana1@gmail.com>
2019-05-19 16:44:14 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
 }
 
 function deleteFromObjectByKey(object, key){
-  const myObject = Object.assign({}, object);
+  const myObject = Object.assign({}, object); // copy object
   delete myObject[key];
   return myObject;
 }
